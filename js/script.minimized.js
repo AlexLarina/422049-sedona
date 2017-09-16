126d2d086419c55338c89a9c05082d77e324c4f3
Alexandra Larina <aa.larina.ff@gmail.com>
2017-09-16 21:57:50 +0300

    Styleguide checked

--- js/script.minimized.js
+++ js/script.minimized.js
@@ -1,1 +1,1 @@
-var indexForm = document.querySelector(".modal-form");var indexButton = document.querySelector(".index-form-button");indexForm.classList.remove("modal-show");indexButton.addEventListener("click", function(e){e.preventDefault();indexForm.classList.toggle("modal-show");});
+/*var indexForm = document.querySelector(".modal-form");var indexButton = document.querySelector(".index-form-button");indexForm.classList.remove("modal-show");indexButton.addEventListener("click", function(e){e.preventDefault();indexForm.classList.toggle("modal-show");});*/
